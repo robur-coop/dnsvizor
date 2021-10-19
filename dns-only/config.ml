025a9c55445fe8a9397c83719268e6f373470632
Robur <team@robur.coop>
2021-10-19 15:08:30 +0000

    dns stub resolver: adapt to dns 6.0.0 API

--- dns-only/config.ml
+++ dns-only/config.ml
@@ -2,26 +2,48 @@
 
 open Mirage
 
-let upstream_resolver =
+let dns_upstream =
   let doc = Key.Arg.info ~doc:"Upstream DNS resolver IP" ["dns-upstream"] in
   Key.(create "dns-upstream" Arg.(opt (some ip_address) None doc))
+
+let dns_port =
+  let doc = Key.Arg.info ~doc:"Upstream DNS resolver port" ["dns-port"] in
+  Key.(create "dns-port" Arg.(opt int 53 doc))
+
+let tls_hostname =
+  let doc = Key.Arg.info ~doc:"Hostname to use for TLS authentication" ["tls-hostname"] in
+  Key.(create "tls-hostname" Arg.(opt (some string) None doc))
+
+let tls_cert_fp =
+  let doc = Key.Arg.info ~doc:"TLS certificate fingerprint" ["tls-cert-fingerprint"] in
+  Key.(create "tls-cert-fingerprint" Arg.(opt (some string) None doc))
+
+let tls_key_fp =
+  let doc = Key.Arg.info ~doc:"TLS public key fingerprint" ["tls-key-fingerprint"] in
+  Key.(create "tls-key-fingerprint" Arg.(opt (some string) None doc))
+
+let no_tls =
+  let doc = Key.Arg.info ~doc:"Disable DNS-over-TLS" ["no-tls"] in
+  Key.(create "no-tls" Arg.(opt bool false doc))
 
 let dnsvizor =
   let packages =
     [
       package "logs" ;
       package "metrics" ;
-      package ~sublibs:["mirage"] "dns-stub";
+      package ~min:"6.0.0" ~sublibs:["mirage"] "dns-stub";
       package "dns";
       package "dns-client";
       package "dns-mirage";
       package "dns-resolver";
       package "dns-tsig";
       package "dns-server";
+      package "ca-certs-nss";
+      package "hex";
     ]
   in
   foreign
-    ~keys:[Key.abstract upstream_resolver]
+    ~keys:[Key.abstract dns_upstream ; Key.abstract dns_port ; Key.abstract tls_hostname ; Key.abstract tls_cert_fp ; Key.abstract tls_key_fp ; Key.abstract no_tls]
     ~packages
     "Unikernel.Main"
     (random @-> pclock @-> mclock @-> time @-> stackv4v6 @-> job)
--- dns-only/unikernel.ml
+++ dns-only/unikernel.ml
@@ -1,18 +1,76 @@
+
+let argument_error = 64
+
 module Main (R : Mirage_random.S) (P : Mirage_clock.PCLOCK)
     (M : Mirage_clock.MCLOCK)
     (Time : Mirage_time.S) (S : Mirage_stack.V4V6) = struct
 
   module Stub = Dns_stub_mirage.Make(R)(Time)(P)(M)(S)
+  module Ca_certs = Ca_certs_nss.Make(P)
 
   let start () () () () s =
     let stub_t =
-      let nameserver = Key_gen.dns_upstream ()
+      let nameservers =
+        match Key_gen.dns_upstream () with
+        | None -> None
+        | Some ip ->
+          if Key_gen.no_tls () then
+            Some ([ `Plaintext (ip, Key_gen.dns_port ()) ])
+          else
+            let time () = Some (Ptime.v (P.now_d_ps ())) in
+            let of_fp data =
+              let hash, fp =
+                let h_of_string = function
+                  | "md5" -> Some `MD5
+                  | "sha" | "sha1" -> Some `SHA1
+                  | "sha224" -> Some `SHA224
+                  | "sha256" -> Some `SHA256
+                  | "sha384" -> Some `SHA384
+                  | "sha512" -> Some `SHA512
+                  | _ -> None
+                in
+                match String.split_on_char ':' data with
+                | [] -> Logs.err (fun m -> m "empty fingerprint") ; exit argument_error
+                | [ fp ] -> `SHA256, fp
+                | hash :: rt -> match h_of_string (String.lowercase_ascii hash) with
+                  | Some h -> h, String.concat "" rt
+                  | None -> Logs.err (fun m -> m "unknown hash: %s" hash); exit argument_error
+              in
+              hash, try Hex.to_cstruct (`Hex fp) with
+                    | Invalid_argument msg ->
+                      Logs.err (fun m -> m "couldn't decode hex string %S: %s" fp msg);
+                      exit argument_error
+            in
+            let authenticator =
+              match Key_gen.tls_cert_fingerprint (), Key_gen.tls_key_fingerprint () with
+              | None, None ->
+                (match Ca_certs.authenticator () with
+                 | Ok auth -> auth
+                 | Error `Msg msg ->
+                   Logs.err (fun m -> m "error retrieving ca certs: %s" msg);
+                   exit argument_error)
+              | Some _, Some _ ->
+                Logs.err (fun m -> m "both certificate and key fingerprint provided");
+                exit argument_error
+              | Some fp, None ->
+                let hash, fingerprint = of_fp fp in
+                X509.Authenticator.server_cert_fingerprint ~time ~hash ~fingerprint
+              | None, Some fp ->
+                let hash, fingerprint = of_fp fp in
+                X509.Authenticator.server_key_fingerprint ~time ~hash ~fingerprint
+            in
+            let peer_name, ip' = match Key_gen.tls_hostname () with
+              | None -> None, Some ip
+              | Some h -> Some (try Domain_name.(host_exn (of_string_exn h)) with Invalid_argument msg -> Logs.err (fun m -> m "invalid host name %S: %s" h msg); exit argument_error), None
+            in
+            let tls = Tls.Config.client ~authenticator ?peer_name ?ip:ip' () in
+            Some [ `Tls (tls, ip, if Key_gen.dns_port () = 53 then 853 else Key_gen.dns_port ()) ]
       and primary_t =
       (* setup DNS server state: *)
         Dns_server.Primary.create ~rng:Mirage_crypto_rng.generate Dns_trie.empty
       in
       (* setup stub forwarding state and IP listeners: *)
-      Stub.create ?nameserver primary_t s
+      Stub.create ?nameservers primary_t s
     in
     let _ = stub_t in
 
