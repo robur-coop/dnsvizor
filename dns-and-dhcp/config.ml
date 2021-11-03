cd65b1e3d958636fecea1e5a800449dad63f0243
Hannes Mehnert <hannes@mehnert.org>
2021-11-03 09:23:03 +0100

    dns-and-dhcp: update to recent DNS API changes

--- dns-and-dhcp/config.ml
+++ dns-and-dhcp/config.ml
@@ -30,16 +30,36 @@ let dhcp_end =
   in
   Key.(create "dhcp-end" Arg.(opt (some ipv4_address) None doc))
 
-let upstream_resolver =
+let dns_upstream =
   let doc = Key.Arg.info ~doc:"Upstream DNS resolver IP" ["dns-upstream"] in
   Key.(create "dns-upstream" Arg.(opt (some ip_address) None doc))
+
+let dns_port =
+  let doc = Key.Arg.info ~doc:"Upstream DNS resolver port" ["dns-port"] in
+  Key.(create "dns-port" Arg.(opt int 53 doc))
+
+let tls_hostname =
+  let doc = Key.Arg.info ~doc:"Hostname to use for TLS authentication" ["tls-hostname"] in
+  Key.(create "tls-hostname" Arg.(opt (some string) None doc))
+
+let tls_cert_fp =
+  let doc = Key.Arg.info ~doc:"TLS certificate fingerprint" ["tls-cert-fingerprint"] in
+  Key.(create "tls-cert-fingerprint" Arg.(opt (some string) None doc))
+
+let tls_key_fp =
+  let doc = Key.Arg.info ~doc:"TLS public key fingerprint" ["tls-key-fingerprint"] in
+  Key.(create "tls-key-fingerprint" Arg.(opt (some string) None doc))
+
+let no_tls =
+  let doc = Key.Arg.info ~doc:"Disable DNS-over-TLS" ["no-tls"] in
+  Key.(create "no-tls" Arg.(opt bool false doc))
 
 let dnsvizor =
   let packages =
     [
       package "logs" ;
       package "metrics" ;
-      package ~sublibs:["mirage"] "dns-stub";
+      package ~min:"6.0.0" ~sublibs:["mirage"] "dns-stub";
       package "dns";
       package "dns-client";
       package "dns-mirage";
@@ -51,6 +71,8 @@ let dnsvizor =
       package ~sublibs:["ipv4"; "tcp"; "udp"; "icmpv4"; "stack-direct"; "ipv6"] "tcpip";
       package "charrua";
       package "charrua-server";
+      package "ca-certs-nss";
+      package "hex";
     ]
   in
   foreign
@@ -58,7 +80,9 @@ let dnsvizor =
            Key.abstract ipv6; Key.abstract ipv6_gateway; Key.abstract ipv6_only;
            Key.abstract accept_router_advertisements;
            Key.abstract dhcp_start; Key.abstract dhcp_end;
-           Key.abstract upstream_resolver]
+           Key.abstract dns_upstream; Key.abstract dns_port;
+           Key.abstract tls_hostname; Key.abstract tls_cert_fp;
+           Key.abstract tls_key_fp; Key.abstract no_tls]
     ~packages
     "Unikernel.Main"
     (random @-> pclock @-> mclock @-> time @-> network @-> job)
