d4dba21b0429a5afd63debe05bf2eab1055f1690
Hannes Mehnert <hannes@mehnert.org>
2022-10-27 12:18:01 +0200

    dns-only: adapt to dns 6.4.0

--- dns-only/config.ml
+++ dns-only/config.ml
@@ -3,43 +3,25 @@
 open Mirage
 
 let dns_upstream =
-  let doc = Key.Arg.info ~doc:"Upstream DNS resolver IP" ["dns-upstream"] in
-  Key.(create "dns-upstream" Arg.(opt (some ip_address) None doc))
-
-let dns_port =
-  let doc = Key.Arg.info ~doc:"Upstream DNS resolver port" ["dns-port"] in
-  Key.(create "dns-port" Arg.(opt int 53 doc))
-
-let tls_hostname =
-  let doc = Key.Arg.info ~doc:"Hostname to use for TLS authentication" ["tls-hostname"] in
-  Key.(create "tls-hostname" Arg.(opt (some string) None doc))
-
-let authenticator =
-  let doc = Key.Arg.info ~doc:"TLS authenticator" ["authenticator"] in
-  Key.(create "authenticator" Arg.(opt (some string) None doc))
-
-let no_tls =
-  let doc = Key.Arg.info ~doc:"Disable DNS-over-TLS" ["no-tls"] in
-  Key.(create "no-tls" Arg.(opt bool false doc))
+  let doc = Key.Arg.info ~doc:"Upstream DNS resolver" ["dns-upstream"] in
+  Key.(create "dns-upstream" Arg.(opt (some string) None doc))
 
 let dnsvizor =
   let packages =
     [
       package "logs" ;
       package "metrics" ;
-      package ~min:"6.0.0" ~sublibs:["mirage"] "dns-stub";
+      package ~min:"6.4.0" ~sublibs:["mirage"] "dns-stub";
       package "dns";
       package "dns-client";
       package "dns-mirage";
       package "dns-resolver";
       package "dns-tsig";
       package "dns-server";
-      package "ca-certs-nss";
-      package ~min:"0.16.0" "x509";
     ]
   in
   foreign
-    ~keys:[Key.v dns_upstream ; Key.v dns_port ; Key.v tls_hostname ; Key.v authenticator ; Key.v no_tls]
+    ~keys:[Key.v dns_upstream ]
     ~packages
     "Unikernel.Main"
     (random @-> pclock @-> mclock @-> time @-> stackv4v6 @-> job)
--- dns-only/unikernel.ml
+++ dns-only/unikernel.ml
@@ -6,40 +6,11 @@ module Main (R : Mirage_random.S) (P : Mirage_clock.PCLOCK)
     (Time : Mirage_time.S) (S : Tcpip.Stack.V4V6) = struct
 
   module Stub = Dns_stub_mirage.Make(R)(Time)(P)(M)(S)
-  module Ca_certs = Ca_certs_nss.Make(P)
 
   let start () () () () s =
     let stub_t =
       let nameservers =
-        match Key_gen.dns_upstream () with
-        | None -> None
-        | Some ip ->
-          if Key_gen.no_tls () then
-            Some ([ `Plaintext (ip, Key_gen.dns_port ()) ])
-          else
-            let authenticator =
-              match Key_gen.authenticator () with
-              | None ->
-                (match Ca_certs.authenticator () with
-                 | Ok auth -> auth
-                 | Error `Msg msg ->
-                   Logs.err (fun m -> m "error retrieving ca certs: %s" msg);
-                   exit argument_error)
-              | Some str ->
-                match X509.Authenticator.of_string str with
-                | Error `Msg msg ->
-                  Logs.err (fun m -> m "%s" msg);
-                  exit argument_error
-                | Ok auth ->
-                  let time () = Some (Ptime.v (P.now_d_ps ())) in
-                  auth time
-            in
-            let peer_name, ip' = match Key_gen.tls_hostname () with
-              | None -> None, Some ip
-              | Some h -> Some (try Domain_name.(host_exn (of_string_exn h)) with Invalid_argument msg -> Logs.err (fun m -> m "invalid host name %S: %s" h msg); exit argument_error), None
-            in
-            let tls = Tls.Config.client ~authenticator ?peer_name ?ip:ip' () in
-            Some [ `Tls (tls, ip, if Key_gen.dns_port () = 53 then 853 else Key_gen.dns_port ()) ]
+        Option.map (fun ns -> [ ns ]) (Key_gen.dns_upstream ())
       and primary_t =
       (* setup DNS server state: *)
         Dns_server.Primary.create ~rng:Mirage_crypto_rng.generate Dns_trie.empty
