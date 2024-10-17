11875a9f4af5c34c48e8f18b761e73f2a7acbfa2
Auto-OCamlformat <auto@ocaml.format>
2024-10-17 09:44:10 +0000

    formatted code

--- config.ml
+++ config.ml
@@ -5,31 +5,33 @@
 open Mirage
 
 let dnsvizor =
-  let packages = [
-    package "logs" ;
-    package "metrics" ;
-    package "dns";
-    package "dns-client";
-    package "dns-mirage";
-    package ~sublibs:["mirage"] "dns-resolver";
-    package ~sublibs:["mirage"] "dns-stub";
-    package "dns-tsig";
-    package "dns-server";
-    package ~min:"3.0.0" "ethernet";
-    package ~min:"3.0.0" ~sublibs:["mirage"] "arp";
-    package ~min:"7.0.0" ~sublibs:["ipv4"; "tcp"; "udp"; "icmpv4"; "stack-direct"; "ipv6"] "tcpip";
-    package "charrua";
-    package "charrua-server";
-    package ~min:"4.5.0" ~sublibs:["network"] "mirage-runtime";
-  ]
+  let packages =
+    [
+      package "logs";
+      package "metrics";
+      package "dns";
+      package "dns-client";
+      package "dns-mirage";
+      package ~sublibs:[ "mirage" ] "dns-resolver";
+      package ~sublibs:[ "mirage" ] "dns-stub";
+      package "dns-tsig";
+      package "dns-server";
+      package ~min:"3.0.0" "ethernet";
+      package ~min:"3.0.0" ~sublibs:[ "mirage" ] "arp";
+      package ~min:"7.0.0"
+        ~sublibs:[ "ipv4"; "tcp"; "udp"; "icmpv4"; "stack-direct"; "ipv6" ]
+        "tcpip";
+      package "charrua";
+      package "charrua-server";
+      package ~min:"4.5.0" ~sublibs:[ "network" ] "mirage-runtime";
+    ]
   in
-  main
-    ~packages
-    "Unikernel.Main"
+  main ~packages "Unikernel.Main"
     (random @-> pclock @-> mclock @-> time @-> network @-> job)
 
 let () =
-  register "dnsvizor" [
-    dnsvizor $ default_random $ default_posix_clock
-    $ default_monotonic_clock $ default_time
-    $ default_network ]
+  register "dnsvizor"
+    [
+      dnsvizor $ default_random $ default_posix_clock $ default_monotonic_clock
+      $ default_time $ default_network;
+    ]
