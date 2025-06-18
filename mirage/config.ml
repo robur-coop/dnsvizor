1a6f306e20950ff032fd867c47dd07cb0b1a58ce
Hannes Mehnert <hannes@mehnert.org>
2025-06-18 08:36:11 +0200

    Add monitoring support to the unikernel
    Also, setup a cirrus job which builds a monitoring unikernel

--- mirage/config.ml
+++ mirage/config.ml
@@ -41,4 +41,73 @@ let mynetwork =
     [ (`Unix, netif ~group:"unix" "tap0") ]
     ~default:default_network
 
-let () = register "dnsvizor" [ dnsvizor $ mynetwork $ assets ]
+let enable_monitoring =
+  let doc = Key.Arg.info
+      ~doc:"Enable monitoring (syslog, metrics to influx, log level, statmemprof tracing)"
+      [ "enable-monitoring" ]
+  in
+  Key.(create "enable-monitoring" Arg.(flag doc))
+
+let management_stack =
+  if_impl
+    (Key.value enable_monitoring)
+    (generic_stackv4v6 ~group:"management" (netif ~group:"management" "management"))
+    (generic_stackv4v6 mynetwork)
+
+let name =
+  runtime_arg ~pos:__POS__
+    {|let doc = Cmdliner.Arg.info ~doc:"Name of the unikernel"
+        ~docs:Mirage_runtime.s_log [ "name" ]
+      in
+      Cmdliner.Arg.(value & opt string "robur.coop" doc)|}
+
+let monitoring =
+  let monitor = Runtime_arg.(v (monitor None)) in
+  let connect _ modname = function
+    | [ stack ; name ; monitor ] ->
+      code ~pos:__POS__
+        "Lwt.return (match %s with\
+         | None -> Logs.warn (fun m -> m \"no monitor specified, not outputting statistics\")\
+         | Some ip -> %s.create ip ~hostname:%s %s)"
+        monitor modname name stack
+    | _ -> assert false
+  in
+  impl
+    ~packages:[ package ~min:"0.0.6" "mirage-monitoring" ]
+    ~runtime_args:[ name ; monitor ]
+    ~connect "Mirage_monitoring.Make"
+    (stackv4v6 @-> job)
+
+let syslog =
+  let syslog = Runtime_arg.(v (syslog None)) in
+  let connect _ modname = function
+    | [ stack ; name ; syslog ] ->
+      code ~pos:__POS__
+        "Lwt.return (match %s with\
+         | None -> Logs.warn (fun m -> m \"no syslog specified, dumping on stdout\")\
+         | Some ip -> Logs.set_reporter (%s.create %s ip ~hostname:%s ()))"
+        syslog modname stack name
+    | _ -> assert false
+  in
+  impl
+    ~packages:[ package ~sublibs:["mirage"] ~min:"0.5.0" "logs-syslog" ]
+    ~runtime_args:[ name ; syslog ]
+    ~connect "Logs_syslog_mirage.Udp"
+    (stackv4v6 @-> job)
+
+let optional_monitoring stack =
+  if_impl (Key.value enable_monitoring)
+    (monitoring $ stack)
+    noop
+
+let optional_syslog stack =
+  if_impl (Key.value enable_monitoring)
+    (syslog $ stack)
+    noop
+
+let () =
+  register "dnsvizor" [
+    optional_syslog management_stack ;
+    optional_monitoring management_stack ;
+    dnsvizor $ mynetwork $ assets
+  ]
