0064a4983836208ffdca2a6bd28be6068148a11b
Automated ocamlformat GitHub action, developed by robur.coop <autoformat@robur.coop>
2025-06-18 06:41:54 +0000

    formatted code

--- mirage/config.ml
+++ mirage/config.ml
@@ -42,8 +42,11 @@ let mynetwork =
     ~default:default_network
 
 let enable_monitoring =
-  let doc = Key.Arg.info
-      ~doc:"Enable monitoring (syslog, metrics to influx, log level, statmemprof tracing)"
+  let doc =
+    Key.Arg.info
+      ~doc:
+        "Enable monitoring (syslog, metrics to influx, log level, statmemprof \
+         tracing)"
       [ "enable-monitoring" ]
   in
   Key.(create "enable-monitoring" Arg.(flag doc))
@@ -51,7 +54,8 @@ let enable_monitoring =
 let management_stack =
   if_impl
     (Key.value enable_monitoring)
-    (generic_stackv4v6 ~group:"management" (netif ~group:"management" "management"))
+    (generic_stackv4v6 ~group:"management"
+       (netif ~group:"management" "management"))
     (generic_stackv4v6 mynetwork)
 
 let name =
@@ -64,50 +68,45 @@ let name =
 let monitoring =
   let monitor = Runtime_arg.(v (monitor None)) in
   let connect _ modname = function
-    | [ stack ; name ; monitor ] ->
-      code ~pos:__POS__
-        "Lwt.return (match %s with\
-         | None -> Logs.warn (fun m -> m \"no monitor specified, not outputting statistics\")\
-         | Some ip -> %s.create ip ~hostname:%s %s)"
-        monitor modname name stack
+    | [ stack; name; monitor ] ->
+        code ~pos:__POS__
+          "Lwt.return (match %s with| None -> Logs.warn (fun m -> m \"no \
+           monitor specified, not outputting statistics\")| Some ip -> \
+           %s.create ip ~hostname:%s %s)"
+          monitor modname name stack
     | _ -> assert false
   in
   impl
     ~packages:[ package ~min:"0.0.6" "mirage-monitoring" ]
-    ~runtime_args:[ name ; monitor ]
-    ~connect "Mirage_monitoring.Make"
+    ~runtime_args:[ name; monitor ] ~connect "Mirage_monitoring.Make"
     (stackv4v6 @-> job)
 
 let syslog =
   let syslog = Runtime_arg.(v (syslog None)) in
   let connect _ modname = function
-    | [ stack ; name ; syslog ] ->
-      code ~pos:__POS__
-        "Lwt.return (match %s with\
-         | None -> Logs.warn (fun m -> m \"no syslog specified, dumping on stdout\")\
-         | Some ip -> Logs.set_reporter (%s.create %s ip ~hostname:%s ()))"
-        syslog modname stack name
+    | [ stack; name; syslog ] ->
+        code ~pos:__POS__
+          "Lwt.return (match %s with| None -> Logs.warn (fun m -> m \"no \
+           syslog specified, dumping on stdout\")| Some ip -> \
+           Logs.set_reporter (%s.create %s ip ~hostname:%s ()))"
+          syslog modname stack name
     | _ -> assert false
   in
   impl
-    ~packages:[ package ~sublibs:["mirage"] ~min:"0.5.0" "logs-syslog" ]
-    ~runtime_args:[ name ; syslog ]
-    ~connect "Logs_syslog_mirage.Udp"
+    ~packages:[ package ~sublibs:[ "mirage" ] ~min:"0.5.0" "logs-syslog" ]
+    ~runtime_args:[ name; syslog ] ~connect "Logs_syslog_mirage.Udp"
     (stackv4v6 @-> job)
 
 let optional_monitoring stack =
-  if_impl (Key.value enable_monitoring)
-    (monitoring $ stack)
-    noop
+  if_impl (Key.value enable_monitoring) (monitoring $ stack) noop
 
 let optional_syslog stack =
-  if_impl (Key.value enable_monitoring)
-    (syslog $ stack)
-    noop
+  if_impl (Key.value enable_monitoring) (syslog $ stack) noop
 
 let () =
-  register "dnsvizor" [
-    optional_syslog management_stack ;
-    optional_monitoring management_stack ;
-    dnsvizor $ mynetwork $ assets
-  ]
+  register "dnsvizor"
+    [
+      optional_syslog management_stack;
+      optional_monitoring management_stack;
+      dnsvizor $ mynetwork $ assets;
+    ]
