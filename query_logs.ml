1f708f790b41f814c19bdc70255c47af3310dd9c
Hannes Mehnert <hannes@mehnert.org>
2025-11-30 19:11:52 +0100

    query log: improve: block button works, there are up to 100 entries

--- query_logs.ml
+++ query_logs.ml
@@ -23,187 +23,61 @@ let query_page =
                     h2
                       ~a:[ a_class [ "text-xl font-bold text-cyan-800 mb-4" ] ]
                       [ txt "Recent Queries (showing up to 100 queries)" ];
-                    a
-                      ~a:
-                        [
-                          a_href "/all-queries";
-                          a_class
-                            [
-                              "text-xl bg-cyan-800 text-white \
-                               hover:bg-cyan-600 p-2 rounded-md font-semibold";
-                            ];
-                        ]
-                      [ txt "Show all queries" ];
-                  ];
-                label
-                  ~a:
-                    [
-                      a_class [ "mb-4 flex items-center space-x-2" ];
-                      a_label_for "select-entries";
-                    ]
-                  [
-                    span [ txt "Show" ];
-                    select
-                      ~a:
-                        [
-                          a_class [ "rounded-md p-1 border border-cyan-800" ];
-                          a_name "entries_per_page";
-                          a_aria "label" [ "Entries per page" ];
-                          a_id "select-entries";
-                        ]
-                      (List.map
-                         (fun n ->
-                           option
-                             ~a:[ a_value (string_of_int n) ]
-                             (txt (string_of_int n)))
-                         [ 10; 25; 50; 100 ]);
-                    span [ txt "entries" ];
-                  ];
-                div
-                  ~a:[ a_class [ "overflow-x-auto" ] ]
-                  [
-                    table
-                      ~a:
-                        [
-                          a_id "query-logs";
-                          a_class
-                            [ "w-full table-auto border-collapse text-sm" ];
-                        ]
-                      ~thead:
-                        (thead
-                           [
-                             tr
-                               (List.map
-                                  (fun heading ->
-                                    th
-                                      ~a:
-                                        [
-                                          a_class
-                                            [
-                                              "border-b p-2 text-left \
-                                               font-semibold text-gray-700";
-                                            ];
-                                        ]
-                                      [ txt heading ])
-                                  [
-                                    "Time";
-                                    "Type";
-                                    "Domain";
-                                    "Client";
-                                    "Return code";
-                                    "Time taken";
-                                    "Status";
-                                    "Action";
-                                  ]);
-                           ])
-                      [
-                        tr
-                          [
-                            td
-                              ~a:[ a_class [ "p-2 text-green-800" ] ]
-                              [ txt "2018-12-19 17:49:46" ];
-                            td ~a:[ a_class [ "p-2" ] ] [ txt "A" ];
-                            td
-                              ~a:
-                                [
-                                  a_class [ "p-2 text-green-800" ];
-                                  a_id "domain-name";
-                                ]
-                              [ txt "next.robur.coop" ];
-                            td ~a:[ a_class [ "p-2" ] ] [ txt "192.168.1.131" ];
-                            td
-                              ~a:[ a_class [ "p-2 text-green-800" ] ]
-                              [ txt "NoError" ];
-                            td
-                              ~a:[ a_class [ "p-2 text-green-800" ] ]
-                              [ txt "25ms" ];
-                            td
-                              ~a:[ a_class [ "p-2 text-green-800" ] ]
-                              [ txt "OK" ];
-                            td
-                              ~a:[ a_class [ "p-2" ] ]
-                              [
-                                form
-                                  ~a:
-                                    [
-                                      a_action "/blocklist/add";
-                                      a_method `Post;
-                                      a_enctype "multipart/form-data";
-                                      a_class [ "flex mb-4 gap-2" ];
-                                      a_onsubmit
-                                        "document.getElementById('domain-input').value \
-                                         = \
-                                         document.getElementById('domain-name').innerText";
-                                    ]
-                                  [
-                                    input
-                                      ~a:
-                                        [
-                                          a_input_type `Hidden;
-                                          a_name "domain";
-                                          a_id "domain-input";
-                                        ]
-                                      ();
-                                    button
-                                      ~a:
-                                        [
-                                          a_class
-                                            [
-                                              "text-white border font-semibold \
-                                               hover:bg-red-100 \
-                                               hover:text-red-800 bg-red-800 \
-                                               rounded p-2 cursor-pointer";
-                                            ];
-                                        ]
-                                      [ txt "Block" ];
-                                  ];
-                              ];
-                          ];
-                        (* TODO: Additional rows would be appended dynamically *)
-                      ];
-                  ];
-                div
-                  ~a:[ a_class [ "mt-4 text-sm" ] ]
-                  [
-                    input
-                      ~a:
-                        [
-                          a_name "apply_filtering";
-                          a_input_type `Checkbox;
-                          a_checked ();
-                          a_aria "label" [ "Apply Filtering" ];
-                        ]
-                      ();
-                    label
-                      ~a:[ a_class [ "ml-2" ] ]
-                      [
-                        txt
-                          "Apply filtering on click on Type, Domain, and \
-                           Clients";
-                      ];
-                  ];
-                div
-                  ~a:[ a_class [ "mt-6 flex justify-between items-center" ] ]
-                  [
-                    span [ txt "Showing 1 to 10 of 100 entries" ];
-                    div
-                      ~a:
-                        [
-                          a_class
-                            [ "space-x-1 flex justify-between items-center" ];
-                        ]
-                      [
-                        button
-                          ~a:[ a_class [ pagination_css ] ]
-                          [ txt "Previous" ];
-                        List.init 5 (fun i ->
-                            button
-                              ~a:[ a_class [ pagination_css ] ]
-                              [ txt (string_of_int (i + 1)) ])
-                        |> Tyxml.Html.div;
-                        button ~a:[ a_class [ pagination_css ] ] [ txt "Next" ];
-                      ];
                   ];
               ];
+            div
+              ~a:[ a_class [ "overflow-x-auto" ] ]
+              [
+                table
+                  ~a:
+                    [
+                      a_id "query-logs";
+                      a_class [ "w-full table-auto border-collapse text-sm" ];
+                    ]
+                  ~thead:
+                    (thead
+                       [
+                         tr
+                           (List.map
+                              (fun heading ->
+                                th
+                                  ~a:
+                                    [
+                                      a_class
+                                        [
+                                          "border-b p-2 text-left \
+                                           font-semibold text-gray-700";
+                                        ];
+                                    ]
+                                  [ txt heading ])
+                              [
+                                "Time";
+                                "Type";
+                                "Domain";
+                                "Client";
+                                "Return code";
+                                "Time taken";
+                                "Status";
+                                "Action";
+                              ]);
+                       ])
+                  [];
+              ];
+          ];
+        div
+          ~a:[ a_class [ "mt-4 text-sm" ] ]
+          [
+            input
+              ~a:
+                [
+                  a_name "apply_filtering";
+                  a_input_type `Checkbox;
+                  a_checked ();
+                  a_aria "label" [ "Apply Filtering" ];
+                ]
+              ();
+            label
+              ~a:[ a_class [ "ml-2" ] ]
+              [ txt "Apply filtering on click on Type, Domain, and Clients" ];
           ];
       ])
